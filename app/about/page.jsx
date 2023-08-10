c5090ae785af88e22c0209023bdabfe05a250ab8
juancjerezm <juanchob61@gmail.com>
2023-08-10 15:25:45 -0500

    Primer Commit

---
+++ app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
+                cupiditate suscipit, aut veniam odit amet asperiores a totam,
+                beatae excepturi nisi officiis, voluptatibus eligendi voluptatum
+                assumenda eius. Sint aspernatur, dolore accusantium nisi culpa
+                in obcaecati quos consectetur deserunt accusamus suscipit
+                quibusdam, dicta corporis earum aliquid eaque quasi quidem
+                eveniet possimus veritatis quisquam quis. Rem excepturi
+                cupiditate libero accusamus, nemo facilis fugiat repellat modi
+                odio facere, recusandae expedita? Laboriosam nostrum nesciunt
+                rem ea odio eligendi explicabo deserunt autem libero? Quod,
+                nostrum praesentium! Voluptate, numquam! Nam, voluptates magni
+                blanditiis incidunt, repudiandae, veniam sint delectus
+                voluptatibus ad sequi et? At corrupti doloremque fugit?
+            </p>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ServicesPage() {
+    return (
+        <div>
+            <h1>services</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+                dolorem sapiente illum magnam, aliquid eos modi suscipit rerum
+                amet harum reiciendis, blanditiis quis consectetur repellat,
+                perspiciatis quibusdam nobis corporis vero fugit magni inventore
+                dicta vel! Facilis, pariatur veritatis corporis excepturi porro
+                officia ullam facere cupiditate, odio minima, ut illo id!
+            </p>
+        </div>
+    );
+}
+
+export default ServicesPage;
